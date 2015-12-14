be0c5eeea9ead7b990af509e5f6ec105bd4ef69b
jatanton <i.thinkimaybebatman@gmail.com>
2015-12-14 15:54:08 +0000

    added association for articles - user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,8 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
+    #hard coded user for now
+    @article.user = User.first
     
     if @article.save
       flash[:success] = "Article was saved"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   
+  has_many :articles
+  # before saving to db bs will convert all email to downcase
+  before_save { self.email = email.downcase }
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
             length: { minimum: 3, maximum: 25 }
